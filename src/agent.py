de5d773d34eeda4bc503577c5c24147e179cddf6
tfriezz <roswall.tim@gmail.com>
2025-06-16 14:07:49 +0900

    add Agent

--- src/agent.py
+++ src/agent.py
@@ -4,26 +4,34 @@ from google import genai
 from google.genai import types
 
 
-def call_agent(user_prompt, verbose=False):
-    load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
+class Agent:
+    def __init__(self, user_prompt):
+        self.user_prompt = user_prompt
+        load_dotenv()
+        self.__api_key = os.environ.get("GEMINI_API_KEY")
+        self.__client = genai.Client(api_key=self.__api_key)
 
-    messages = [
-        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-    ]
+        self.messages = [
+            types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        ]
 
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=messages,
-    )
+        self.response = self.__client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=self.messages,
+        )
 
-    if not verbose:
-        print(response.text)
-        # print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        # print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    if verbose:
-        print(f"User prompt: {user_prompt}\n")
-        print(f"{response.text}\n")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    def call_agent(self, verbose=False):
+        response = self.__client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=self.messages,
+        )
+
+        if not verbose:
+            print(response.text)
+            # print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            # print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if verbose:
+            print(f"User prompt: {self.user_prompt}\n")
+            print(f"{response.text}\n")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
--- src/main.py
+++ src/main.py
@@ -3,13 +3,16 @@ import sys
 # import os
 # from dotenv import load_dotenv
 # from google import genai
-from agent import call_agent
+from agent import Agent
+
+agent = Agent(sys.argv[1])
 
 
 if len(sys.argv) > 2 and sys.argv[2] == "--verbose":
-    call_agent(sys.argv[1], True)
+    agent.call_agent(verbose=True)
+
 elif len(sys.argv) > 1:
-    call_agent(sys.argv[1])
+    agent.call_agent()
 
 elif not len(sys.argv) > 1:
     raise ValueError("No prompt provided")
